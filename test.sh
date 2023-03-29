422f6c5a0325b8a82c2c9a80f3ba15ea29d578c7
Ole Morud <82065181+olemorud@users.noreply.github.com>
2023-03-29 13:10:57 +0200

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-data='{"Message":"coffee", "Channel_id":"abc", "Channel_name":"Town Square", "Team_domain":"abc", "Team_id":"abc", "Post_id":"post id", "Text":"coffee", "Timestamp":"0", "Token":'$TEST_TOKEN'}'
-
-curl -m 70 -X POST https://europe-west1-rising-city-366608.cloudfunctions.net/coffee \
--H "Authorization: bearer $(gcloud auth print-identity-token)" \
--H "Content-Type: application/json" \
--d "$data"
